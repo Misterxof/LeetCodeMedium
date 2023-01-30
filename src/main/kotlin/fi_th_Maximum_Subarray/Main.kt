9421b6039e5685ca30d868e31a9b58eadb6d8706
Misterxof <mishail.lukushevich@gmail.com>
2023-01-31 00:35:29 +0300

    53. Maximum Subarray
    Maximum Subarray

--- src/main/kotlin/fi_th_Maximum_Subarray/Main.kt
+++ src/main/kotlin/fi_th_Maximum_Subarray/Main.kt
@@ -2,7 +2,8 @@ package t_o_se_Contains_Duplicate
 
 
 fun main(args: Array<String>) {
-  //  println(Solution().maxSubArray(intArrayOf(-2,1,-3,4,-1,2,1,-5,4)))
-//    println(Solution().maxSubArray(intArrayOf(-1)))
+//    println(Solution().maxSubArray(intArrayOf(0,1,2,3,)))
+    println(Solution().maxSubArray(intArrayOf(-2,1,-3,4,-1,2,1,-5,4)))
+    println(Solution().maxSubArray(intArrayOf(-1)))
     println(Solution().maxSubArray(intArrayOf(-2,-1)))
 }
--- src/main/kotlin/fi_th_Maximum_Subarray/Solution.kt
+++ src/main/kotlin/fi_th_Maximum_Subarray/Solution.kt
@@ -2,23 +2,59 @@ package t_o_se_Contains_Duplicate
 
 class Solution {
     fun maxSubArray(nums: IntArray): Int {
+        var maxSum = Int.MIN_VALUE
+        var previousSum = 0
+
+        nums.forEachIndexed { index, i ->
+            val currentSum = previousSum + i
+
+            previousSum = if (currentSum < i) i else currentSum
+
+            if (maxSum < previousSum)
+                maxSum = previousSum
+        }
+
+        return maxSum
+    }
+
+    fun maxSubArray1(nums: IntArray): Int {
         var sum = Int.MIN_VALUE
 
-        nums.forEachIndexed {index, i ->
-            for (j in index..(nums.size)){
-                var sumT = 0
-                if (index != j){
-                    sumT = nums.copyOfRange(index, j).sum() // 1..collection.size, 0 0 returns nothing
+        nums.forEachIndexed { index, i ->
+            var sumT = 0
+            for (j in index..(nums.size - 1)) {
 
-                    if (sumT > sum)
-                        sum = sumT
-                }
+                sumT += nums[j]
 
+                if (sumT > sum)
+                    sum = sumT
 
-                println("i=$index j=$j sumT=$sumT sum=$sum")
             }
         }
 
         return sum
+    }
+
+    fun maxSubArray2(nums: IntArray): Int {
+        var list: MutableList<Int> = mutableListOf()
+
+        nums.forEachIndexed { index, i ->
+            var size = list.size
+            var j = index
+
+            if (size == 0) {
+                list.add(i)
+                return@forEachIndexed
+            }
+
+            while (j != 0) {
+                list.add(i + list[size - j])
+                j--
+            }
+
+            list.add(i)
+        }
+
+        return list.max()
     }
 }
