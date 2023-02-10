4e737591f263eadbb8027ea88666054d94288160
Mikhail <mishail.lukushevich@gmail.com>
2023-02-10 21:23:45 +0300

    1162. As Far from Land as Possible
    As Far from Land as Possible
    - Runtime 451 ms - 47.6%
    - Memory 54.5 MB - 41.18%

--- src/main/kotlin/o_o_s_t_As_Far_Land_Possible/AsFarLandPossible.kt
+++ src/main/kotlin/o_o_s_t_As_Far_Land_Possible/AsFarLandPossible.kt
@@ -1,68 +1,87 @@
 package o_o_s_t_As_Far_Land_Possible
 
+import java.util.Arrays.copyOf
 import kotlin.math.abs
 
 class AsFarLandPossible {
+    var distance = 0
+
     fun maxDistance(grid: Array<IntArray>): Int {
-        val hashMap = hashMapOf<Int, MutableList<Pair<Int, Int>>>()
-        val distances = hashMapOf<String, MutableSet<Int>>()
-        val minimals = mutableSetOf<Int>()
-        var min = 10000
+        if (!containsWater(grid) || !containsLand(grid)) return -1
+        var result = Array(grid.size) { IntArray(grid[0].size) }
+        var grid = grid
+
+        do {
+            grid.forEachIndexed { i, it ->
+                it.forEachIndexed { j, jt ->
+                    if (jt == 1) {
+                        result[i][j] = 1
+                        if (j - 1 >= 0)
+                            result[i][j - 1] = 1
+                        if (j + 1 <= it.size - 1)
+                            result[i][j + 1] = 1
+                        if (i - 1 >= 0)
+                            result[i - 1][j] = 1
+                        if (i + 1 <= grid.size - 1)
+                            result[i + 1][j] = 1
+                    }
+                }
+            }
+            grid = result
+            result = cloneArray(grid)
+            distance++
+        } while (containsWater(result))
+
+        return distance
+    }
+
+    private fun cloneArray(grid: Array<IntArray>): Array<IntArray> {
+        val result = Array(grid.size) { IntArray(grid[0].size) }
 
         grid.forEachIndexed { i, it ->
             it.forEachIndexed { j, jt ->
-                if (hashMap.containsKey(jt))
-                    hashMap[jt]!!.add(i to j)
-                else
-                    hashMap[jt] = mutableListOf(i to j)
+                result[i][j] = jt
             }
         }
 
-        if (!hashMap.containsKey(0) || !hashMap.containsKey(1)) return -1
+        return result
+    }
 
-        println(hashMap)
-
-
-        hashMap[0]!!.forEach {
-            distances[it.toString()] = mutableSetOf()
-            hashMap[1]!!.forEach { jt ->
-                distances[it.toString()]!!.add(abs(it.first - jt.first) + abs(it.second - jt.second))
-
-            }
+    private fun containsWater(grid: Array<IntArray>): Boolean {
+        grid.forEach {
+            if (it.contains(0)) return true
         }
 
-        distances.forEach {
-            it.value.forEach {jt ->
-                min = minOf(min, jt)
-            }
-            minimals.add(min)
-            min = 10000
+        return false
+    }
+
+    private fun containsLand(grid: Array<IntArray>): Boolean {
+        grid.forEach {
+            if (it.contains(1)) return true
         }
 
-        println(distances)
-
-        return minimals.maxOf { it }
+        return false
     }
 }
 
 fun main() {
     println(AsFarLandPossible().maxDistance(arrayOf(
-            intArrayOf(1,0,1),
-            intArrayOf(0,0,0),
-            intArrayOf(1,0,1)
+            intArrayOf(1, 0, 1),
+            intArrayOf(0, 0, 0),
+            intArrayOf(1, 0, 1)
     )))
     println()
     println(AsFarLandPossible().maxDistance(arrayOf(
-            intArrayOf(1,0,0),
-            intArrayOf(0,0,0),
-            intArrayOf(0,0,0)
+            intArrayOf(1, 0, 0),
+            intArrayOf(0, 0, 0),
+            intArrayOf(0, 0, 0)
     )))
     println()
     println(AsFarLandPossible().maxDistance(arrayOf(
-            intArrayOf(0,0,1,1,1),
-            intArrayOf(0,1,1,0,0),
-            intArrayOf(0,0,1,1,0),
-            intArrayOf(1,0,0,0,0),
-            intArrayOf(1,1,0,0,1)
+            intArrayOf(0, 0, 1, 1, 1),
+            intArrayOf(0, 1, 1, 0, 0),
+            intArrayOf(0, 0, 1, 1, 0),
+            intArrayOf(1, 0, 0, 0, 0),
+            intArrayOf(1, 1, 0, 0, 1)
     )))
 }
