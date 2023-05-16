8060aa38611f2f43b3836a99d69658f01f9a5497
Mikhail <mishail.lukushevich@gmail.com>
2023-05-16 19:40:50 +0300

    Linked List Cycle II Update
    Linked List Cycle II with Two pointers

--- src/main/kotlin/o_f_t_Linked_List_Cycle_II/LinkedListCycleII.kt
+++ src/main/kotlin/o_f_t_Linked_List_Cycle_II/LinkedListCycleII.kt
@@ -1,31 +1,58 @@
 package o_f_t_Linked_List_Cycle_II
 
-import tw_Add_Two_Numbers.ListNode
 
 class LinkedListCycleII {
-    fun detectCycle(head: Array<Int>) {
-        val list1 = ListNode(head)
-        list1.getListNodeCircle(list1.firstNode, 1)
-    }
+//    fun detectCycle(head: Array<Int>) {
+//        val list1 = ListNode(head)
+//        list1.getListNodeCircle(list1.firstNode, 1)
+//    }
 
     // LeetCode solution
-    // fun detectCycle(head: ListNode?): ListNode? {
-    //        var node = head
-    //        var set: HashSet<ListNode> = HashSet()
-    //
-    //        while(node != null) {
-    //            if (!set.add(node))
-    //                return node
-    //            else
-    //                set.add(node!!)
-    //
-    //            node = node.next
-    //        }
-    //
-    //        return null
-    //    }
+    fun detectCycle2(head: ListNode?): ListNode? {
+        var node = head
+        var set: HashSet<ListNode> = HashSet()
+
+        while (node != null) {
+            if (!set.add(node))
+                return node
+            else
+                set.add(node!!)
+
+            node = node.next
+        }
+
+        return null
+    }
+
+    // Two pointers
+    fun detectCycle(head: ListNode?): ListNode? {
+        var slowPointer = head
+        var fastPointer = head
+
+        while (slowPointer != null && fastPointer != null) {
+            slowPointer = slowPointer?.next
+            fastPointer = fastPointer?.next?.next
+
+            if (slowPointer == fastPointer)
+                break
+        }
+
+        if (fastPointer == null) return null
+        slowPointer = head
+
+        while (slowPointer != fastPointer) {
+            slowPointer = slowPointer?.next
+            fastPointer = fastPointer?.next
+        }
+
+        return slowPointer
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
 }
 
 fun main() {
-    LinkedListCycleII().detectCycle(arrayOf(3,2,0,-4))
+//    LinkedListCycleII().detectCycle(arrayOf(3, 2, 0, -4))
 }
