ed2d7955e57639b17424f5bb3db3e051f59eda99
Mikhail <mishail.lukushevich@gmail.com>
2025-01-03 13:18:29 +0300

    2270. Number of Ways to Split Array
    2270. Number of Ways to Split Array
    - Runtime 25 ms - ?%
    - Memory 66.53 MB - 12.50%

---
+++ src/main/kotlin/t_t_se_e_z_Number_Ways_Split_Array/NumberWaysSplitArray.kt
@@ -0,0 +1,41 @@
+package t_t_se_e_z_Number_Ways_Split_Array
+
+class NumberWaysSplitArray {
+    fun waysToSplitArray(nums: IntArray): Int {
+        val leftSum = LongArray(nums.size)
+        val rightSum = LongArray(nums.size)
+        var sum = 0L
+
+//        if (nums.size == 2) {
+//            return if (nums[0] >= nums[1]) 1 else 0
+//        }
+
+        nums.forEachIndexed { index, i ->
+            sum += i
+            leftSum[index] = sum
+        }
+
+        sum = 0
+
+        for (i in nums.lastIndex downTo 0) {
+            sum += nums[i]
+            rightSum[i] = sum
+        }
+
+        sum = 0
+
+        for (i in 0 until nums.lastIndex) {
+//            println("l ${leftSum[i]} r ${rightSum[i]}")
+            if (leftSum[i] >= rightSum[i + 1]) sum++
+        }
+
+        return sum
+    }
+}
+
+fun main() {
+    println(NumberWaysSplitArray().waysToSplitArray(intArrayOf(10,4,-8,7)))
+    println(NumberWaysSplitArray().waysToSplitArray(intArrayOf(2,3,1,0)))
+    println(NumberWaysSplitArray().waysToSplitArray(intArrayOf(2, 3, 4)))
+    println(NumberWaysSplitArray().waysToSplitArray(intArrayOf(0, -1, -2, -3, -4)))
+}
