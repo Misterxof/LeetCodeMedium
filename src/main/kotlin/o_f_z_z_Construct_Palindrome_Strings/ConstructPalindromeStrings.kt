ee1878257742db904da4783a82f444a451599294
Mikhail <mishail.lukushevich@gmail.com>
2025-01-11 20:18:44 +0300

    1400. Construct K Palindrome Strings
    1400. Construct K Palindrome Strings
    - Runtime 12 ms - 70.00%
    - Memory 39.48 MB - 30.00%

---
+++ src/main/kotlin/o_f_z_z_Construct_Palindrome_Strings/ConstructPalindromeStrings.kt
@@ -0,0 +1,20 @@
+package o_f_z_z_Construct_Palindrome_Strings
+
+class ConstructPalindromeStrings {
+    fun canConstruct(s: String, k: Int): Boolean {
+        val lattesArray = IntArray(25)
+        var oddCount = 0
+
+        if (s.length < k) return false
+
+        s.forEach {
+            lattesArray[it - 'a' - 1] += 1
+        }
+
+        for (i in lattesArray.indices) {
+            if (lattesArray[i] % 2 != 0) oddCount++
+        }
+
+        return oddCount <= k
+    }
+}
